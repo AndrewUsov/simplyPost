59e42d3425e0951f56b2ecae7a074ab7b1f0dc61
Andrew Usov <leo30001@yandex.ru>
2021-12-17 03:02:41 +1000

    add give 5 func

--- Sources/simplyPost/simplyPost.swift
+++ Sources/simplyPost/simplyPost.swift
@@ -3,4 +3,11 @@ public struct simplyPost {
 
     public init() {
     }
+    
+    
+    /// easy peasy funcciya
+    /// - Returns: returnit stro4ku
+    public func getFive() -> String {
+        return "this is your five"
+    }
 }
